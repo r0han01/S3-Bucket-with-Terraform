929a53a5c6e2e040916aafbdb8c771ec570a9a13
r0han01 <accounts@rkatkam.com>
2024-12-23 23:06:02 -0600

    PRIVACY MAINTAINED FILES

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+# Variables for AWS credentials
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+# Variable for tag value
+variable "tag_value" {
+  description = "The value for the tag"
+  type        = string
+}
+
+# S3 bucket resource
+resource "aws_s3_bucket" "r0han" {
+  bucket = "my-unique-r0han"  # Use a unique name
+  tags = {
+    "num" = var.tag_value  # Use the variable in the tag
+  }
+}
